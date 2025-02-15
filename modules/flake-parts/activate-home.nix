d0e973c02c2c1ebcb9e64297bee36861a2493cf8
Stefan Mayer <stevijo.mayer@googlemail.com>
2025-02-15 11:14:24 +0100

    lint nix files

--- modules/flake-parts/activate-home.nix
+++ modules/flake-parts/activate-home.nix
@@ -1,25 +1,27 @@
 {
-  perSystem = { self', pkgs, lib, ... }: let
-    default ={
-      inherit (self'.packages.activate) meta;
-      program = pkgs.writeShellApplication {
-        name = "activate-home";
+  perSystem = { self', pkgs, lib, ... }:
+    let
+      default = {
+        inherit (self'.packages.activate) meta;
+        program = pkgs.writeShellApplication {
+          name = "activate-home";
 
-        text = ''
-          set -e
-          exit_code=0
-          grep NAME=NixOS /etc/os-release || exit_code=$?
-          if [[ $exit_code -eq 0 || $# -ne 0 ]]; then
-            ${lib.getExe self'.packages.activate} "$@";
-          else
-            ${lib.getExe self'.packages.activate} "$USER"@"$(hostname -s)";
-          fi
-        '';
+          text = ''
+            set -e
+            exit_code=0
+            grep NAME=NixOS /etc/os-release || exit_code=$?
+            if [[ $exit_code -eq 0 || $# -ne 0 ]]; then
+              ${lib.getExe self'.packages.activate} "$@";
+            else
+              ${lib.getExe self'.packages.activate} "$USER"@"$(hostname -s)";
+            fi
+          '';
+        };
       };
+    in
+    {
+      # Enables 'nix run' to activate home-manager config.
+      apps.default = default;
+      packages.default = default;
     };
-  in {
-    # Enables 'nix run' to activate home-manager config.
-    apps.default = default;
-    packages.default = default;
-  };
 }
